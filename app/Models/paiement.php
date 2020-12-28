10a7dcae5a7ddae2abc6dc3593835d577b525c19
DaoDjack <73937577+DaoDjack@users.noreply.github.com>
2020-12-28 10:18:45 +0000

    CRM

--- app/Models/paiement.php
+++ app/Models/paiement.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class produit extends Model
+class paiement extends Model
 {
     use HasFactory;
-
-    protected $table = "produit";
-    
-    protected $fillable = ['produitId', 'libelle', 'prixUnitaire', 'agenceId'];
 }
---
+++ database/migrations/2020_12_23_123327_create_produits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produit', function (Blueprint $table) {
+            $table->id();
+            $table->string('produitId');
+            $table->string('libelle');
+            $table->int('prixUnitaire');
+            $table->string('agenceId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
